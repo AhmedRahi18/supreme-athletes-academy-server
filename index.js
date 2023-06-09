17f4a8b5fc3d03ed3fbf7e71e32e3c12a6ddd540
AhmedRahi <rezarahi71@gmail.com>
2023-06-09 20:52:36 +0600

    classes get

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const classCollection = client.db('academy').collection('classes')
+
+    app.get('/classes',async(req,res)=>{
+        const result = await classCollection.find({}).sort({enrolled: -1}).toArray()
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
