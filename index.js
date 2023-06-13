8eaf7df4b9d32ad2d301c3a220c3fcb98fd5f4ca
AhmedRahi <rezarahi71@gmail.com>
2023-06-13 21:39:05 +0600

    delete added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -52,6 +52,23 @@ async function run() {
       const result = await myClassCollection.insertOne(query);
       res.send(result);
     });
+
+    app.get('/myClasses',async(req,res)=>{
+      const email = req.query.email;
+      if(!email){
+        res.send([])
+      }
+      const query = {email: email};
+      const result = await myClassCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.delete('/myClasses/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await myClassCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // all Instructors 
     app.get('/instructors', async (req, res) => {
