262e3c76dc97a08941bee4b43207dccc8c073446
AhmedRahi <rezarahi71@gmail.com>
2023-06-15 00:26:47 +0600

    instructor added

--- index.js
+++ index.js
@@ -46,6 +46,17 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/instructor/:id',async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{
+          role: 'instructor'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
 
     app.post("/users", async (req, res) => {
       const user = req.body;
